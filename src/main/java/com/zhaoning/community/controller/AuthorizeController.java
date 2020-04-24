15615bbef3343cc5da550cf8c979831d93d1851a
zhaoning <366111937@qq.com>
2020-04-24 18:05:37 +0800

    将变量放入配置文件

--- src/main/java/com/zhaoning/community/controller/AuthorizeController.java
+++ src/main/java/com/zhaoning/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.zhaoning.community.dto.AccessTokenDTO;
 import com.zhaoning.community.dto.GithubUser;
 import com.zhaoning.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,15 +19,24 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.client.uri}")
+    private String clientUrl;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name = "state") String state){
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("c5e159d8e1416d50b5f2");
-        accessTokenDTO.setClient_secret("6aa32b7fe56068fa699acff3879fe1485625f242");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setRedirect_uri(clientUrl);
         accessTokenDTO.setState(state);
 
         String accessToker = githubProvider.getAccessToker(accessTokenDTO);

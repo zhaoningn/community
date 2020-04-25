f0a4909f0965afbb28f055f61c33f13d6ddee4eb
zhaoning <366111937@qq.com>
2020-04-25 17:26:06 +0800

    实现持久化登录

--- src/main/java/com/zhaoning/community/controller/IndexController.java
+++ src/main/java/com/zhaoning/community/controller/IndexController.java
@@ -1,7 +1,13 @@
 package com.zhaoning.community.controller;
 
+import com.zhaoning.community.mapper.UserMapper;
+import com.zhaoning.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author zhaoning
@@ -10,8 +16,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        for(Cookie cookie:cookies){
+            if (cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                User user=userMapper.fingByToken(token);
+                if (user!=null){
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
+
+
         return "index";
     }
 }
--- src/main/java/com/zhaoning/community/mapper/UserMapper.java
+++ src/main/java/com/zhaoning/community/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package com.zhaoning.community.mapper;
 import com.zhaoning.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Component;
 
 /**
@@ -16,4 +18,6 @@ public interface UserMapper {
     @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
      void insert(User user);
 
+    @Select("select * from user where token = #{token}")
+    User fingByToken(@Param("token") String token);
 }
